abd9c1d35464c637a1347b9638d02ee6fa56e265
wassim <kadhiwassim@gmail.com>
2022-09-22 20:06:32 +0100

    mern backend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +21,7 @@ function App() {
         </a>
       </header>
     </div>
+    
   );
 }
 
