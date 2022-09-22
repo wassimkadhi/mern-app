48776711601366421bb68db0c4ac947f0f153217
wassimkadhi <113668306+wassimkadhi@users.noreply.github.com>
2022-09-22 20:31:09 +0100

    Update dbConfig.js
    Modified user name password

--- backend/dbConfig.js
+++ backend/dbConfig.js
@@ -1,7 +1,7 @@
 
 
 const mongoose =require('mongoose') ;
-const uri ="mongodb+srv://kadhiwassim:academie@test.d2zj3pw.mongodb.net/?retryWrites=true&w=majority" ;
+const uri ="mongodb+srv://username:password@test.d2zj3pw.mongodb.net/?retryWrites=true&w=majority" ;
 mongoose.connect(uri , {useNewUrlParser: true , useUnifiedTopology:true}); 
 const connection=mongoose.connection;
 connection.once('open' , ()=> {console.log("data base connection succefully");})
